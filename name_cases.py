e49af729cecec96c10355afc8f1a73712191a53b
wikilike7 <wikilike7@gmail.com>
2018-07-18 17:30:08 +0800

    initial commit

---
+++ name_cases.py
@@ -0,0 +1,16 @@
+name = 'Frod'
+expr = ', would you like to learn some Python today ?'
+hi = 'Hello '
+print(hi + name + expr)
+print(hi + name.lower() + expr)
+print(hi + name.upper() + expr)
+
+famous_person = 'Steven Jobs'
+message = 'once said, "We\'re here to put a dent in the universe. Otherwise why else even be here?"'
+print(famous_person + message)
+
+name_frod = '\tFrod\n\tBaggins   '
+print(name_frod)
+print(name_frod.lstrip())
+print(name_frod.rstrip())
+print(name_frod.strip())
