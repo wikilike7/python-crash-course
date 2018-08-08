1f3445bdb11dc0619d8ccea9bace340db8ff3731
wikilike7 <wikilike7@gmail.com>
2018-08-08 17:23:47 +0800

    update chapter 4

---
+++ chap 4/4.2-try.py
@@ -0,0 +1,3 @@
+pizzas = ['French Bread', 'New York Slice', 'Chicago Pizza']
+for pizza in pizzas:
+    print()
--- chap 4/magicians.py
+++ chap 4/magicians.py
@@ -1,4 +1,5 @@
 magicians = ['alice', 'david', 'caroliane']
 for magician in magicians:
-    print(magician)
-
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+print("Thank you, everyone. That was a great magic show!")
