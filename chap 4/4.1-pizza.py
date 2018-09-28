a3b02193bc19326a954094a0ccb88e9f50462607
wikilike7 <wikilike7@gmail.com>
2018-09-28 17:17:55 +0800

    finish excise 4.1 & 4.2

--- chap 4/4.1-pizza.py
+++ chap 4/4.1-pizza.py
@@ -1,3 +1,4 @@
 pizzas = ['French Bread', 'New York Slice', 'Chicago Pizza']
 for pizza in pizzas:
-    print()
+    print('I like ' + pizza + ' pizza')
+print('I really love pizza!')
---
+++ chap 4/4.2-animals.py
@@ -0,0 +1,4 @@
+felidae = ['acinonyx jubatus', 'felis catus', 'puma concolor']
+for animal in felidae:
+    print('A' + animal + 'would make a great pet')
+print('Any of these animals would make a great pet!')
