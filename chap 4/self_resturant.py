d3d0442be97e2e6c0afac9cfddac7eb9c64e918e
wikilike7 <wikilike7@gmail.com>
2018-10-09 16:05:25 +0800

    update

--- chap 4/self_resturant.py
+++ chap 4/self_resturant.py
@@ -3,9 +3,7 @@ foods = ('cake', 'apple', 'banana', 'beer', 'cafe')
 for food in foods:
     print(food)
 
-# foods[0] = 'blackberry'
-# print(foods)
-
 foods = ('blackberry', 'pizza', 'banana', 'beer', 'cafe')
 for food in foods:
     print(food)
+
