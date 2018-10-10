b88ca2e5b9d200502a05640100634425c5863524
wikilike7 <wikilike7@gmail.com>
2018-10-10 09:30:02 +0800

    updates in chap5

---
+++ chap5/conditional_tests.py
@@ -0,0 +1,13 @@
+food = 'apple'
+another_food = 'banana'
+foods = ['apple', 'blackberry', 'cafe']
+age = 19
+
+print(food == another_food)
+print(food in foods)
+
+if age > 18:
+    print('You are old enough to vote.')
+else:
+    print('Sorry, you are too young to vote.')
+
