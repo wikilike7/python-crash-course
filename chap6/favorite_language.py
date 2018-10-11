45039f4a8cd51f9a5c785717d927809f44b6a549
wikilike7 <wikilike7@gmail.com>
2018-10-11 17:25:48 +0800

    update

---
+++ chap6/favorite_language.py
@@ -0,0 +1,8 @@
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print('Sarah\'s favorite language is ' + favorite_language['sarah'].title() + '.')
---
+++ chap6/people_info.py
@@ -0,0 +1,32 @@
+# 6.1
+
+people_info = {
+    'first_name': 'shichao',
+    'last_name': 'wang',
+    'age': '21',
+    'city': 'nanjing'
+}
+
+for person in people_info:
+    print(person + ': ' + people_info[person])
+
+# 6.2
+
+favorite_numbers = {
+    'zibba': '13',
+    'Andy': '1',
+    'Amy': '2',
+}
+
+for favorite_number in favorite_numbers:
+    print(favorite_number + '\'s favorite number is ' + favorite_numbers[favorite_number])
+
+#  6.3
+
+vocabulary = {
+    'variable': 'present a placehoder or container to fill in data',
+    'Array': 'an orded data set',
+    'Tuple': 'same with Array but can\'t changed',
+    'Data type': 'present what kind of the data, like integer, string, float',
+    'condition statement': 'if condition',
+}
