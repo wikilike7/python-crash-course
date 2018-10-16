a47b1c67bfc871387cca021ff60ad73ed12e1052
wikilike7 <wikilike7@gmail.com>
2018-10-16 14:19:31 +0800

    updates

---
+++ chap7/7-3.py
@@ -0,0 +1,7 @@
+user_input = input('Enter a number: ')
+user_input = int(user_input)
+
+if user_input % 2 == 0:
+    print('The number is integer multiple of 10')
+else:
+    print('Not int multiple')
